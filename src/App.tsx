43d336cf9e5b2bab142eccf2c92fd392f3f92a16
vtrivellato <victor.trivellato@hotmail.com>
2023-03-04 23:45:59 -0300

    adjusted css file for App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Header } from "./components/Header/Header"
 import { Post } from "./components/Post/Post"
 import { Sidebar } from "./components/Sidebar/Sidebar"
 
-import './App.modules.css'
+import './App.module.css'
 
 import './global.css'
 
